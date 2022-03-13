496795ede877e91e20ec7f3e6734d351d69ef5a9
Jens Pfeifle <jens@pfeifle.tech>
2022-03-13 21:00:36 +0100

    Patch for ESP-IDF compatibility.

--- src/sys/unix.rs
+++ src/sys/unix.rs
@@ -84,7 +84,7 @@ pub fn time_to_local_tm(sec: i64, tm: &mut Tm) {
         if libc::localtime_r(&sec, &mut out).is_null() {
             panic!("localtime_r failed: {}", io::Error::last_os_error());
         }
-        #[cfg(any(target_os = "solaris", target_os = "illumos"))]
+        #[cfg(any(target_os = "solaris", target_os = "illumos", target_os = "espidf"))]
         let gmtoff = {
             tzset();
             // < 0 means we don't know; assume we're not in DST.
